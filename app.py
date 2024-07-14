abe1a2bfcd8b87f1e0f2b27fda932267fb290c10
Daisuke <grande.demais.montanha@gmail.com>
2024-07-14 21:33:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,35 @@ def update_urine_data():
     plot_urine_analysis(data)
     st.dataframe(data)
 
+# 行動回数をカウントする関数
+def count_actions(data):
+    data['Start Time'] = pd.to_datetime(data['Start Time'])
+    data['End Time'] = pd.to_datetime(data['End Time'])
+    data['Duration (s)'] = pd.to_numeric(data['Duration (s)'])
+    
+    actions = ['drinking', 'defecating', 'urinating']
+    action_counts = {action: 0 for action in actions}
+
+    for action in actions:
+        action_data = data[data['Action'] == action]
+        if not action_data.empty:
+            action_data = action_data.sort_values('Start Time').reset_index(drop=True)
+            action_count = 0
+            ongoing = False
+            for i in range(len(action_data)):
+                if action_data.loc[i, 'Duration (s)'] >= 1:
+                    if not ongoing:
+                        action_count += 1
+                        ongoing = True
+                    else:
+                        if i > 0 and (action_data.loc[i, 'Start Time'] - action_data.loc[i-1, 'End Time']).total_seconds() > 0.5:
+                            action_count += 1
+            action_counts[action] = action_count
+
+    st.subheader('Count of Specific Actions')
+    for action, count in action_counts.items():
+        st.write(f'{action}: {count} times')
+
 # 行動時間の合計をプロットする関数
 def plot_action_durations(data):
     data['Start Time'] = pd.to_datetime(data['Start Time'])
@@ -39,14 +68,14 @@ def plot_action_durations(data):
     for action, duration in action_durations.items():
         st.write(f'{action}: {duration} seconds')
 
-    # # 棒グラフのプロット
-    # fig, ax = plt.subplots()
-    # action_durations.plot(kind='bar', ax=ax)
-    # plt.xlabel('Action')
-    # plt.ylabel('Total Duration (s)')
-    # plt.title('Total Duration of Each Action Over the Day')
-    # plt.xticks(rotation=45)
-    # st.pyplot(fig)
+    # 棒グラフのプロット
+    fig, ax = plt.subplots()
+    action_durations.plot(kind='bar', ax=ax)
+    plt.xlabel('Action')
+    plt.ylabel('Total Duration (s)')
+    plt.title('Total Duration of Each Action Over the Day')
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
 
 # 各時刻に対する各行動の積算時間をプロットする関数
 def plot_cumulative_action_durations(data):
@@ -89,6 +118,8 @@ elif page == "Behavior Analysis":
     st.write("This section provides an analysis of the dog's behavior data.")
     if st.button('Update Behavior Data'):
         update_behavior_data()
+        data = load_data('https://raw.githubusercontent.com/Daisuke7155/dog_monitoring/main/action_durations.csv')
+        count_actions(data)
 elif page == "Urinary Analysis":
     st.write("This section provides an analysis of the dog's urine data.")
     if st.button('Update Urine Data'):
